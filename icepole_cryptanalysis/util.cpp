b4734a6d43ca5b0ad561c506845b6856f5c3f3c3
Ran Proshan <ran.proshan@gmail.com>
2018-07-31 10:19:47 +0300

    disable log prep when log disabled

--- icepole_cryptanalysis/util.cpp
+++ icepole_cryptanalysis/util.cpp
@@ -9,43 +9,52 @@
 
 void log_buffer(const char * label, const u_int8_t * buffer, const size_t size, const char * logcat, const int level)
 {
-	std::stringstream srs;
-	srs << std::hex << std::setfill('0');
-	for(size_t i = 0; i < size; ++i)
-		srs << std::setw(2) << static_cast<unsigned>(buffer[i]);
-	log4cpp::Category::getInstance(logcat).log(level, "%s: [%s]", label, srs.str().c_str());
+	if(log4cpp::Category::getInstance(logcat).isPriorityEnabled(level))
+	{
+		std::stringstream srs;
+		srs << std::hex << std::setfill('0');
+		for(size_t i = 0; i < size; ++i)
+			srs << std::setw(2) << static_cast<unsigned>(buffer[i]);
+		log4cpp::Category::getInstance(logcat).log(level, "%s: [%s]", label, srs.str().c_str());
+	}
 }
 
 void log_block(const char * label, const u_int64_t * block, const char * logcat, const int level)
 {
-	std::string str;
-	char buffer[64];
-	for(int i = 0; i < 4; ++i)
+	if(log4cpp::Category::getInstance(logcat).isPriorityEnabled(level))
 	{
-		for(int j = 0; j < 4; ++j)
+		std::string str;
+		char buffer[64];
+		for(int i = 0; i < 4; ++i)
 		{
-			snprintf(buffer, 64, "%016lX ", RC2I(block, i, j));
-			str += buffer;
+			for(int j = 0; j < 4; ++j)
+			{
+				snprintf(buffer, 64, "%016lX ", RC2I(block, i, j));
+				str += buffer;
+			}
+			str += '\n';
 		}
-		str += '\n';
+		log4cpp::Category::getInstance(logcat).log(level, "%s:\n%s", label, str.c_str());
 	}
-	log4cpp::Category::getInstance(logcat).log(level, "%s:\n%s", label, str.c_str());
 }
 
 void log_state(const char * label, const u_int64_t state[4][5], const char * logcat, const int level)
 {
-	std::string str;
-	char buffer[64];
-	for(int i = 0; i < 4; ++i)
+	if(log4cpp::Category::getInstance(logcat).isPriorityEnabled(level))
 	{
-		for(int j = 0; j < 5; ++j)
+		std::string str;
+		char buffer[64];
+		for(int i = 0; i < 4; ++i)
 		{
-			snprintf(buffer, 64, "%016lX ", state[i][j]);
-			str += buffer;
+			for(int j = 0; j < 5; ++j)
+			{
+				snprintf(buffer, 64, "%016lX ", state[i][j]);
+				str += buffer;
+			}
+			str += '\n';
 		}
-		str += '\n';
+		log4cpp::Category::getInstance(logcat).log(level, "%s:\n%s", label, str.c_str());
 	}
-	log4cpp::Category::getInstance(logcat).log(level, "%s:\n%s", label, str.c_str());
 }
 
 u_int64_t left_rotate(u_int64_t v, size_t r)
