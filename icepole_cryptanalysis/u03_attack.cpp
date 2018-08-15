f3b1be9e89aa1d0ebd701c4fe8ed07e44ddc1ce2
Ran Proshan <ran.proshan@gmail.com>
2018-08-15 15:31:15 +0300

    proper form

--- icepole_cryptanalysis/u03_attack.cpp
+++ icepole_cryptanalysis/u03_attack.cpp
@@ -155,9 +155,9 @@ int attack_u03(const char * logcat, const u_int8_t * key, const u_int8_t * iv, u
 								memset(atckr_prms[i].ctrs, 0, 64 * sizeof(bit_ctrs_t));
 								atckr_prms[i].attacks_done = 0;
 								atckr_prms[i].required_attacks = (pow(2, 33.7)/thread_count)+1;
-								//atckr_prms[i].attack = the_attack;
+								atckr_prms[i].attack = the_attack;
 								//atckr_prms[i].attack = the_attack_check;
-								atckr_prms[i].attack = the_attack_hack;
+								//atckr_prms[i].attack = the_attack_hack;
 								if(0 != (errcode = pthread_create(atckr_thds.data() + i, NULL, attacker, (void *)(atckr_prms.data() + i))))
 								{
 									char errmsg[256];
