a73de03fa6557b440f47a8e157452441a2d7f4db
Ran Proshan <ran.proshan@gmail.com>
2018-08-19 17:56:47 +0300

    sym attack dev work

--- icepole_cryptanalysis/util.cpp
+++ icepole_cryptanalysis/util.cpp
@@ -125,3 +125,255 @@ void * attacker(void * arg)
 	log4cpp::Category::getInstance(prm->logcat).debug("%s: exit.", __FUNCTION__);
 	return NULL;
 }
+
+void sigint_cb(evutil_socket_t, short, void * arg)
+{
+	event_param_t * eprm = (event_param_t *)arg;
+	log4cpp::Category::getInstance(eprm->locat).notice("%s: SIGINT caught; breaking event loop.", __FUNCTION__);
+	event_base_loopbreak(eprm->the_base);
+}
+
+void timer_cb(evutil_socket_t, short, void * arg)
+{
+	event_param_t * eprm = (event_param_t *)arg;
+
+	bool all_attacks_done = true;
+	size_t samples_done, attacks_done;
+	for(size_t i = 0; i < thread_count; ++i)
+	{
+		all_attacks_done = all_attacks_done && ((*eprm->atckr_prms)[i].attacks_done >= (*eprm->atckr_prms)[i].required_attacks);
+		log4cpp::Category::getInstance(eprm->locat).notice("%s: thread %lu launched %lu attacks out of prescribed %lu.",
+				__FUNCTION__, i, (*eprm->atckr_prms)[i].attacks_done, (*eprm->atckr_prms)[i].required_attacks);
+	}
+
+	if(all_attacks_done)
+	{
+		log4cpp::Category::getInstance(eprm->locat).notice("%s: all samples are done for all threads; breaking event loop.", __FUNCTION__);
+		event_base_loopbreak(eprm->the_base);
+		return;
+	}
+
+	time_t now = time(NULL);
+	if(now > (eprm->start_time + allotted_time))
+	{
+		log4cpp::Category::getInstance(eprm->locat).info("%s: start=%lu; allotted=%lu; now=%lu;", __FUNCTION__, eprm->start_time, allotted_time, now);
+		log4cpp::Category::getInstance(eprm->locat).notice("%s: allotted time of %lu seconds is up; breaking event loop.", __FUNCTION__, allotted_time);
+		event_base_loopbreak(eprm->the_base);
+		return;
+	}
+}
+
+void get_init_state(u_int64_t is[4][5], const u_int8_t * key, const u_int8_t * iv, const char * logcat)
+{
+	u_int8_t C[128+16];
+	memset(C, 0, 128+16);
+	unsigned long long clen = 128+16;
+
+	u_int8_t P[128] = { 0 };
+
+	crypto_aead_encrypt_i((unsigned char *)C, &clen, (const unsigned char *)P, 128, NULL, 0, NULL, iv, key, is);
+
+	{
+		/*
+		The code below proves that the hacked init state's first four columns are identical
+		to the XOR of a single block with its encryption result P^C (In the specific example here P=0 so C=P^C).
+		Later, when we no longer need crutches, the crypto_aead_encrypt_i(...) will be replaced with
+		a regular crypto_aead_encrypt(...) call and C will be placed inside the first four columns of
+		is and the fifth column will be zeroed out.
+		*/
+		const u_int64_t * pC = (const u_int64_t *)C;
+		for(int i = 0; i < 4; ++i)
+		{
+			for(int j = 0; j < 4; ++j)
+			{
+				if(RC2I(pC,i,j) != is[i][j])
+				{
+					log4cpp::Category::getInstance(logcat).fatal("%s: init state validation failure.", __FUNCTION__);
+					log_state("is", is, logcat, 0);
+					log_block("P", (const u_int64_t *)P, logcat, 0);
+					log_block("C", (const u_int64_t *)C, logcat, 0);
+					exit(-1);
+				}
+			}
+		}
+	}
+}
+
+bool last_Sbox_lookup_filter(const u_int64_t * P_perm_output, const size_t bit_offset,
+							 const block_bit_t * bits, const size_t bit_count,
+							 u_int8_t & F_xor_res, const char * logcat)
+{
+	u_int8_t row_bits, input_bit;
+	F_xor_res = 0;
+
+	for(size_t i = 0; i < bit_count; ++i)
+	{
+		block_bit_t current_bit = bits[i];
+		current_bit.z = (current_bit.z + bit_offset)%64;
+
+		row_bits = get_block_row_bits(P_perm_output, current_bit.x, current_bit.z);
+		input_bit = 0;
+
+		if(lookup_Sbox_input_bit(row_bits, current_bit.y, input_bit))
+			F_xor_res ^= input_bit;
+		else
+			return false;
+	}
+	return true;
+}
+
+u_int8_t get_block_bit(const u_int64_t * P, const size_t x, const size_t y, const size_t z)
+{
+	return (0 != (P[x + 4 * y] & (0x1UL << (z%64))))? 1: 0;
+}
+
+u_int8_t get_block_row_bits(const u_int64_t * P, const size_t x, const size_t z)
+{
+	return (
+			(get_block_bit(P, x, 0, z)	 )	|
+			(get_block_bit(P, x, 1, z) << 1)	|
+			(get_block_bit(P, x, 2, z) << 2)	|
+			(get_block_bit(P, x, 3, z) << 3)
+			);
+}
+
+bool lookup_Sbox_input_bit(const u_int8_t output_row_bits, const size_t input_bit_index, u_int8_t & input_bit)
+{
+	switch(output_row_bits)
+	{
+	case 0x0://in doc
+		switch(input_bit_index)
+		{
+		case 0: input_bit = 1; return true;
+		case 2: input_bit = 1; return true;
+		case 4: input_bit = 1; return true;
+		default: return false;
+		}
+		break;
+	case 0x1://1000
+		return false;
+	case 0x2://0100
+		switch(input_bit_index)
+		{
+		case 0: input_bit = 0; return true;
+		case 1: input_bit = 1; return true;
+		case 3: input_bit = 0; return true;
+		default: return false;
+		}
+		break;
+	case 0x3://1100
+		switch(input_bit_index)
+		{
+		case 0: input_bit = 1; return true;
+		default: return false;
+		}
+		break;
+	case 0x4://0010
+		switch(input_bit_index)
+		{
+		case 1: input_bit = 0; return true;
+		default: return false;
+		}
+		break;
+	case 0x5://1010
+		switch(input_bit_index)
+		{
+		case 1: input_bit = 0; return true;
+		case 3: input_bit = 0; return true;
+		default: return false;
+		}
+		break;
+	case 0x6://0110
+		switch(input_bit_index)
+		{
+		case 0: input_bit = 0; return true;
+		case 1: input_bit = 1; return true;
+		case 3: input_bit = 0; return true;
+		default: return false;
+		}
+		break;
+	case 0x7://1110
+		switch(input_bit_index)
+		{
+		case 0: input_bit = 1; return true;
+		case 1: input_bit = 1; return true;
+		default: return false;
+		}
+		break;
+	case 0x8://0001
+		switch(input_bit_index)
+		{
+		case 0: input_bit = 0; return true;
+		case 1: input_bit = 1; return true;
+		case 2: input_bit = 0; return true;
+		case 3: input_bit = 1; return true;
+		default: return false;
+		}
+		break;
+	case 0x9://1001
+		switch(input_bit_index)
+		{
+		case 0: input_bit = 1; return true;
+		case 2: input_bit = 0; return true;
+		default: return false;
+		}
+		break;
+	case 0xA://0101
+		switch(input_bit_index)
+		{
+		case 0: input_bit = 0; return true;
+		case 1: input_bit = 0; return true;
+		case 2: input_bit = 0; return true;
+		case 3: input_bit = 1; return true;
+		default: return false;
+		}
+		break;
+	case 0xB://1101
+		switch(input_bit_index)
+		{
+		case 0: input_bit = 1; return true;
+		case 2: input_bit = 0; return true;
+		default: return false;
+		}
+		break;
+	case 0xC://0011
+		switch(input_bit_index)
+		{
+		case 0: input_bit = 1; return true;
+		case 1: input_bit = 0; return true;
+		case 2: input_bit = 1; return true;
+		default: return false;
+		}
+		break;
+	case 0xD://1011
+		switch(input_bit_index)
+		{
+		case 0: input_bit = 0; return true;
+		case 1: input_bit = 0; return true;
+		case 2: input_bit = 1; return true;
+		case 3: input_bit = 1; return true;
+		default: return false;
+		}
+		break;
+	case 0xE://0111
+		switch(input_bit_index)
+		{
+		case 0: input_bit = 0; return true;
+		case 1: input_bit = 1; return true;
+		case 2: input_bit = 1; return true;
+		case 3: input_bit = 1; return true;
+		default: return false;
+		}
+		break;
+	case 0xF://1111
+		switch(input_bit_index)
+		{
+		case 3: input_bit = 0; return true;
+		case 4: input_bit = 0; return true;
+		default: return false;
+		}
+		break;
+	default: return false;
+	}
+	return false;
+}
